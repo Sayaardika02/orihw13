cba8245bcb1478403a95b385a5be140a8c12b7b6
sayaardika02 <sayaardika02@gmail.com>
2023-01-31 01:02:39 +0700

    first commit

---
+++ baseikal/command/game/game.js
@@ -0,0 +1,24 @@
+const game =  ` 
+*Game Maker*
+┏━━⊱
+┣❏tebak lagu
+┣❏tebak kata
+┣❏tebak lirik
+┣❏tebak kalimat
+┣❏tebak gambar
+┣❏tebak lontong
+┣❏jadian
+┣❏tictactoe
+┣❏family100
+┣❏math [mode]
+┣❏suitpvp [@tag]
+┣❏leaderboard
+┣❏inventori
+┣❏mining
+┣❏beli
+┣❏jual
+┣❏heal
+┣❏berburu
+┣❏deltt
+┗━━⊱[ 💢𝚁𝚎𝚟𝚘𝚕𝚝💢 ]`
+exports.game = game
